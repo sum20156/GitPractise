23694e35ad06579eba5104831b7464234dc33ca4
Suman Mondal <sum20156@gmail.com>
2021-04-18 11:59:55 +0530

    Hey Friend this is my first commit

--- app/src/main/java/com/example/gitpractise/MainActivity.kt
+++ app/src/main/java/com/example/gitpractise/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val data = getData()
         performOperation(data)
+        println("Hey Friend I am in")
     }
 
     private fun performOperation(data: String) {
