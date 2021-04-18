dd680519098968aa72ebb4059ced64cf00c3cd4c
Suman Mondal <sum20156@gmail.com>
2021-04-18 11:07:58 +0530

    data fetch and processing feature is added

--- app/src/main/java/com/example/gitpractise/MainActivity.kt
+++ app/src/main/java/com/example/gitpractise/MainActivity.kt
@@ -8,6 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val data = getData()
+        performOperation(data)
+    }
 
+    private fun performOperation(data: String) {
+        if (data.equals("Data")){
+            println("Success")
+        }else{
+            throw NullPointerException("Failed")
+        }
+    }
+
+    private fun getData(): String {
+        return "Data"
     }
 }
